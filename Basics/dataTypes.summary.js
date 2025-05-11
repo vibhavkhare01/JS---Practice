bc9de0f4c7a4339eda84ad047161ce2cd9a55d40
Vibhav Khare <152658333+vibhavkhare01@users.noreply.github.com>
2025-05-11 19:49:39 +0530

    New

---
+++ Basics/dataTypes.summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+//    7 types :  String. Number, Boolean, Null, Undefined, symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.2
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 544847874847864684n
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Object, Function
+
+
+const heros = ["shaktiman", "nagraj", "dogesh"];
+let myObj = {
+    name: "Godess",
+    age : 19,
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+
+console.log(typeof bigNumber);

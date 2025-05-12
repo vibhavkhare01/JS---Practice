5ffb306aa042c1308316249815201b7bd783434b
Vibhav Khare <152658333+vibhavkhare01@users.noreply.github.com>
2025-05-12 23:57:47 +0530

    ...

---
+++ Basics/strings.js
@@ -0,0 +1,36 @@
+const name = "Git"
+const repocount = 50
+
+//  console.log(name + repoCount + "Value")
+
+console.log(`Hello my name is ${name} and my repooCunt is ${repocount}`);
+
+const gameName = new String('Godess')
+// In this string is an object.
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName,length);
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newstring = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "     Godess               "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "http://Godess.com/godess%19abc"
+console.log(url.replace('%20','_'))
+
+console.log(url.includes('god'))
+
+console.log(gameName.split('_'));

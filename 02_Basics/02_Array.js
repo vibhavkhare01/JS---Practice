6a51a123496fde6a5791a7bdebc53983970c8836
Vibhav Khare <152658333+vibhavkhare01@users.noreply.github.com>
2025-05-20 19:49:39 +0530

    ///

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,36 @@
+const marvel_heros = ["Thor", "Iron", "Hulk", "Black Widow", "Captain America"];
+const dc_heros = ["Batman", "Superman", "Wonder"]
+
+//marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros);
+//console.log(marvel_heros[5][1]);
+
+const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+//console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7,[4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+
+
+
+
+console.log(Array.isArray("Godess"))
+console.log(Array.from("Godess"))
+console.log(Array.from({Name: "Godess"})) // Interesting
+
+let score1 = 200
+let score2 = 300
+let score3 = 400
+
+console.log(Array.of(score1, score2, score3));

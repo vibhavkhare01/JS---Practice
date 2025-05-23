dbbb2ca070dcf5dba6fa8fc4a7be24ef10b5b5dd
Vibhav Khare <152658333+vibhavkhare01@users.noreply.github.com>
2025-05-24 00:01:52 +0530

    ///

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,27 @@
+function sayMyName(){
+    console.log("G");
+    console.log("o");
+    console.log("d");
+    console.log("e");   
+    console.log("s");
+    console.log("s");
+}
+
+
+// sayMyName()
+
+function addTwoNumbers(num1, num2){
+    //console.log(num1 + num2);
+    
+}
+
+addTwoNumbers(2, 3)  // 5
+addTwoNumbers(2, "3")  // 23
+addTwoNumbers(2, "a")  // NaN
+addTwoNumbers(2, null)  //
+
+
+const result = addTwoNumbers(2, 3)
+
+console.log("result: ", result); // undefined
+

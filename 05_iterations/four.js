d141588ba5ce9c5c5979457152887b8fb241d0cd
Vibhav Khare <152658333+vibhavkhare01@users.noreply.github.com>
2025-05-28 19:58:51 +0530

    ///

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'JavaScript',
+    py: 'Python',
+    rb: 'Ruby',
+    java: 'Java'
+
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ['js', 'py', 'rb', 'java']
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+//const map = new Map()
+//map.set('In', "India")
+//map.set('Us', "United States")
+//map.set('Uk', "United Kingdom")
+map.set('In', "India") 
+
+//for (const key in map) {
+//    console.log(key);
+    
+//}
+
